73274d444ea9d809c7b9c2d6451fe99b4796f31e
Raul Sanchez <raulsf66@gmail.com>
2020-03-30 18:23:38 +0200

    Finished implementation

--- rainbow_attack_parallel.cpp
+++ rainbow_attack_parallel.cpp
@@ -13,7 +13,27 @@ argv[1]: rainbow table file path
 argv[2]: hashes to crack file path
 */
 
+// Abrir en init
+// Copiar el fichero 3 veces
+// Mandarlo
+
 int main (int argc, char* argv[]){
+	
+	int id, numprocs;
+	int size, chunk;
+	int total_cracked;
+	double init_execution;
+	double finish_execution;
+	double total_execution;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	MPI_Status status;
+
+	if (id == 0)
+		init_execution = MPI_Wtime();
+
 	char rainbow_table[50];
 	int table_number = atoi(argv[1]);
 	string rainbow_path(argv[1]);
@@ -21,21 +41,6 @@ int main (int argc, char* argv[]){
 	ifstream file(rainbow_path);
 	string pass, hash;
 	unordered_map<string, string> hashMap;
-	int id, numprocs;
-	int size, chunk;
-	int total_cracked;
-	
-	while(!file.eof()){	//Build Map. BUILD DIFFERENT MAP FOR EACH DIFFERENT THREAD - READ NUMBER OF LINES, DISTRIBUTE THEM BETWEEN PROCESSES
-		file >> pass;
-		file >> hash;
-		hashMap.insert(pair<string, string>(hash,pass));
-	}
-	char str[65];
-
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &id);
-	MPI_Status status;
 
 	vector<string> hashes;
 	string buffer;
@@ -43,6 +48,13 @@ int main (int argc, char* argv[]){
 	int lines;
 	int lines_per_process;
 	int cracked = 0;
+
+	while(!file.eof()){	//Build Map. BUILD DIFFERENT MAP FOR EACH DIFFERENT THREAD - READ NUMBER OF LINES, DISTRIBUTE THEM BETWEEN PROCESSES
+		file >> pass;
+		file >> hash;
+		hashMap.insert(pair<string, string>(hash,pass));
+	}
+	char str[65];
 
 	if (hash_file){
 		//Get file size
@@ -68,23 +80,37 @@ int main (int argc, char* argv[]){
 		string first_hash = hashes[0];
 		string sencond_hash = hashes[1];
 		cout << "SOY " + to_string(id) + " LEO: " + to_string(lines_per_process) + "\n";
-		cout << "SOY " + to_string(id) + " FIRST HASH: " + first_hash + "\n";
-		cout << "SOY " + to_string(id) + " SECOND HASH: " + sencond_hash + "\n";
-		
+
 		//Crack hashes
+		double t_init_calculation = MPI_Wtime();
 		for (auto it = hashes.begin(); it != hashes.end(); ++it){
 			if(searchHash(hashMap, *it))
 				cracked++;
 			
 		}
+		double t_finish_calculation = MPI_Wtime();
+		double t_elapsed_calculation = t_finish_calculation - t_init_calculation;
 
-		//test	
+		
 
 		MPI_Reduce( &cracked, &total_cracked, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-		if (id == 0)
-			cout << "TOTAL CRACKED: " + to_string(total_cracked);
+		cout << "ELAPSED FOR " + to_string(id) + ": " + to_string(t_elapsed_calculation) + "\n";
+		
+		if (id == 0){
+			finish_execution = MPI_Wtime();
+			total_execution = finish_execution - init_execution;
+			cout << "TOTAL CRACKED: " + to_string(total_cracked) + "\n";
+			cout << "TOTAL TIME: " + to_string(total_execution) + "\n";
+		}
 	}
+	else
+	{
+		if (id == 0){
+			cout << "Error reading hash file";
+		}
+	}
+	
 	
 	MPI_Finalize();
 }
